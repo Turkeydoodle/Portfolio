ea1be5100f0ee166f71fd5ffb88f6d17dfe82719
Kevin Todd-Yu <kevintoddyu@gmail.com>
2025-12-06 10:32:52 -0800

    changes so that when mouse enter div1, div1 changes color, when div2 is entered, div3 pops up.

--- test.js
+++ test.js
@@ -1,11 +1,7 @@
 div = document.getElementById("div");
-function divcolor() {
-    div.style.backgroundColor = "green";
-}
 function divreset() {
     div.style.backgroundColor = "blue";
 }
-div.onmouseover = divcolor;
 div.onmouseout = divreset;
 div2 = document.getElementById('div2');
 function divcolor2() {
@@ -16,20 +12,31 @@ function divreset2() {
 }
 function createelement() {
     let dividee = document.createElement("div");
+    div2.style.backgroundColor = "green";
     let node = document.createTextNode("div3");
     dividee.appendChild(node);
-    dividee.style.width = '100px';
-    dividee.style.height = '100px';
+    dividee.id = 'div3';
+    dividee.style.width = '250px';
+    dividee.style.height = '250px';
     dividee.style.backgroundColor = 'yellow';
     dividee.style.color = 'black';
     dividee.style.padding = '8px';
-    dividee.style.border = '2px solid #333';
     dividee.style.position = 'absolute';
-    dividee.style.left = '500px';
-    dividee.style.top = '500px';
+    dividee.style.left = '425px';
+    dividee.style.top = '425px';
     dividee.style.zIndex = '9999';
     document.body.appendChild(dividee);
 }
-div.onclick = createelement;
+function destroyelement() {
+    const e = arguments[0];
+    const related = e && (e.relatedTarget || e.toElement);
+    if (related) {
+        if (related.id === 'div3' || (related.closest && related.closest('#div3'))) return;
+    }
+    const element = document.getElementById('div3');
+    if (element && element.parentNode) element.parentNode.removeChild(element);
+}
 div2.onmouseover = divcolor2;
+div.onmouseover = createelement;
 div2.onmouseout = divreset2;
+div.onmouseout = destroyelement;
