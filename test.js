6b4caf7ca047de68d2809f7671e38e1fc093fadf
Kevin Todd-Yu <kevintoddyu@gmail.com>
2025-12-06 10:14:09 -0800

    Click div2 to make div3 show

--- test.js
+++ test.js
@@ -14,5 +14,22 @@ function divcolor2() {
 function divreset2() {
     div2.style.backgroundColor = "red";
 }
+function createelement() {
+    let dividee = document.createElement("div");
+    let node = document.createTextNode("div3");
+    dividee.appendChild(node);
+    dividee.style.width = '100px';
+    dividee.style.height = '100px';
+    dividee.style.backgroundColor = 'yellow';
+    dividee.style.color = 'black';
+    dividee.style.padding = '8px';
+    dividee.style.border = '2px solid #333';
+    dividee.style.position = 'absolute';
+    dividee.style.left = '500px';
+    dividee.style.top = '500px';
+    dividee.style.zIndex = '9999';
+    document.body.appendChild(dividee);
+}
+div.onclick = createelement;
 div2.onmouseover = divcolor2;
 div2.onmouseout = divreset2;
